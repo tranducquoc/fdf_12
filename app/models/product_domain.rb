a375274420135b59bc67dc652c42bd6460b28cdf
NGUYEN HONG LOI <nguyen.hong.loi@framgia.com>
2017-03-31 14:33:07 +0700

    fixbug_change_shop_user

--- app/models/product_domain.rb
+++ app/models/product_domain.rb
@@ -1,4 +1,7 @@
 class ProductDomain < ApplicationRecord
   belongs_to :product
   belongs_to :domain
+
+  scope :not_in_shop_domain, -> only_shop_domain {where.not domain_id: only_shop_domain}
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -38,8 +38,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://order.framgia.vn' ]
+  
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 

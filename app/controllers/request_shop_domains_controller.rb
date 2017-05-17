bcd0d53821bf0978dacf4ef568a364785299f72f
Bo Rie <macintosh@Bo-Macbook.local>
2017-05-17 08:47:09 +0700

    approve or reject request joins domain

--- app/controllers/request_shop_domains_controller.rb
+++ app/controllers/request_shop_domains_controller.rb
@@ -2,7 +2,7 @@ class RequestShopDomainsController < ApplicationController
   before_action :load_request, only: :update
   before_action :load_shop, only: :index
   before_action :authenticate_user!
-  
+
   def index
     @user_domains = current_user.domains
   end

4cc9efb83d6a4a6da12118cc41554544b2cd1a28
Bo Rie <macintosh@Bo-Macbook.local>
2017-05-25 16:05:29 +0700

    fix new

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var time = 5;
+  var id = '';
   function timeButton(time) {
     var text = I18n.t("automatic_go_to", {time: time});
     $('p.timer').html(text);
@@ -9,11 +10,12 @@ $(document).ready(function() {
     }, 1000);
     if (time == 0) {
       clearTimeout(timer);
-      $('.confirm-no').get(0).click();
+      $('.confirm-no-' + id).get(0).click();
     };
   };
   $('.confirm-button').click(function() {
     time = 5;
+    id = $(this).data('id');
     timeButton(time);
   });
   $('#modal-confirm').on('hidden.bs.modal', function() {

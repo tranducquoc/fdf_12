6d5c5f8be8cf5e625b9cfe3675a32a4a3b8cc1ec
Bo Rie <macintosh@Bo-Macbook.local>
2017-06-02 15:02:19 +0700

    api create all order

--- app/controllers/v1/orders_product_all_controller.rb
+++ app/controllers/v1/orders_product_all_controller.rb
@@ -4,12 +4,13 @@ class V1::OrdersProductAllController < V1::BaseController
   def create
     domain_id = params[:domain_id]
     if domain_id.present?
-      data_orders = []
       data_carts = cart_params
       if data_carts.present?
-        count_order = Settings.count_order_start
-        response_success t("api.success"),
-          OrderProductAllApiService.new(data_carts, count_order, data_orders).order_all_product
+        if OrderProductAllApiService.new(data_carts, current_user).order_all_product
+          response_success t "api.success"
+        else
+          response_error t "api.error"
+        end
       else
         response_not_found t "api.error_orders_list_not_found"
       end
--- app/services/order_product_all_api_service.rb
+++ app/services/order_product_all_api_service.rb
@@ -1,34 +1,40 @@
 class OrderProductAllApiService
-  def initialize data_carts, count_order, data_orders
+  def initialize data_carts, user
     @data_carts = data_carts
-    @count_order = count_order
-    @data_orders = data_orders
+    @current_user = user
   end
 
   def order_all_product
     @data_carts.each do |cart|
-      @count_order = @count_order + Settings.increase_one
-      @data_orders << cart
-      order = Order.new cart.permit(:domain_id, :total_pay, :shop_id,
-        :user_id, :status, :orders)
       ActiveRecord::Base.transaction do
-        order.save
-      end
-      @data_orders[@count_order]["orders"].each do |product|
-        product_order = order.order_products.new product.permit(:status,
-          :product_id, :user_id, :quantity)
-        ActiveRecord::Base.transaction do
-          product_order.save
+        begin
+          order = Order.new cart.permit(:domain_id, :total_pay, :shop_id, :user_id)
+          order.save
+          cart["products"].each do |product|
+            product_order = order.order_products.new product.permit(:product_id,
+              :quantity, :notes).merge! user_id: cart[:user_id]
+            product_order.save
+          end
+          order.create_event_order if checked_notification_setting?(Settings.index_two_in_array)
+        rescue
+          return false
         end
       end
-      event = order.events.new
-      event.message = Settings.notification_new
-      event.user_id = order.shop.owner_id
-      event.eventable_id = order.shop.id
-      event.eventitem_id = order.id
-      ActiveRecord::Base.transaction do
-        event.save
+    end
+    return true
+  end
+
+  private
+  def checked_notification_setting? index
+    if @current_user.notification_settings.present?
+      case @current_user.notification_settings.values[index]
+      when Settings.serialize_false
+        return false
+      when Settings.serialize_true
+        return true
       end
+    else
+      return true
     end
   end
 end

b47aa946524f34ba7c4a5c1740cebb8c5c77509d
CuongNC <cuongnc.dev@gmail.com>
2017-06-05 10:29:18 +0700

    Load list domain public api

--- app/controllers/v1/domains_controller.rb
+++ app/controllers/v1/domains_controller.rb
@@ -5,15 +5,24 @@ class V1::DomainsController < V1::BaseController
 
   def index
     if params[:user_token].present?
-      user_domains = UserDomain.all.select do |t|
-        t.user_id.to_s == params[:user_id]
-      end
-      if user_domains.present?
-        result = ActiveModel::Serializer::CollectionSerializer.new(user_domains,
-          each_serializer: UserDomainSerializer)
-        response_success t("api.success"), result
+      if params.has_key?(:domain_status)
+        domain = Domain.by_status params[:domain_status]
+        if domain.present?
+          response_success t("api.success"), domain
+        else
+          response_not_found t "api.error_domains_not_found"
+        end
       else
-        response_not_found t "api.error_domains_not_found"
+        user_domains = UserDomain.all.select do |t|
+          t.user_id.to_s == params[:user_id]
+        end
+        if user_domains.present?
+          result = ActiveModel::Serializer::CollectionSerializer.new(user_domains,
+            each_serializer: UserDomainSerializer)
+          response_success t("api.success"), result
+        else
+          response_not_found t "api.error_domains_not_found"
+        end
       end
     else
       response_not_found t "api.error_user_token_not_found"

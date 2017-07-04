60799d41f73129523cd18df65ab8d265d417f3d8
CuongNC <cuongnc.dev@gmail.com>
2017-07-04 13:26:41 +0700

    Fix bug update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,31 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def update
+    user = User.friendly.find params[:id]
+    if user.present?
+      if user == current_user
+        if user.valid_password? params[:user][:current_password]
+          user.update_attributes user_params
+          bypass_sign_in user
+          @change_password_status = Settings.status_save.success
+        else
+          @change_password_status = Settings.status_save.error
+        end
+      else
+        flash[:danger] = t "api.error"
+        redirect_to user_path current_user
+      end
+    else
+      flash[:danger] = t "api.error_user_not_found"
+      redirect_to user_path current_user
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
 end

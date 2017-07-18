af21d8437386b5f8574dc7d06f0f94cc30d5f2e5
Nara <leductienttkt@gmail.com>
2017-07-18 13:19:49 +0700

    fix api 18

--- app/controllers/v1/shop_managers_controller.rb
+++ app/controllers/v1/shop_managers_controller.rb
@@ -6,12 +6,8 @@ class V1::ShopManagersController < V1::BaseController
   def index
     shop = Shop.find_by id: params[:shop_id]
     if shop.present?
-      user_managers = shop.shop_managers.select_user_add_role
-      if user_managers.detect {|u| u.id == current_user.id}.present?
-        response_success t("api.success"), user_managers
-      else
-        response_error t "api.error"
-      end
+      user_managers = shop.shop_managers
+      response_list user_managers, ShopManagerSerializer, ""
     else
       response_not_found "shop_not_found"
     end

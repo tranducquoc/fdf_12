a664e4cc3a72491644f040f4f184ce3acf9af106
Nguyen Thi Hao <haonguyen4993@gmail.com>
2018-07-06 17:07:28 +0700

    improve post show page

--- app/models/supports/ads/post_support.rb
+++ app/models/supports/ads/post_support.rb
@@ -25,19 +25,26 @@ class Supports::Ads::PostSupport
   end
 
   def filtered_param
-    category_slug = params[:category_slug] ? params[:category_slug] : Category.is_parent.first.slug
+    category_slug = params[:category_slug]
     category = Category.find_by slug: category_slug
     current_domain = Domain.friendly_id.find_by slug: params[:domain_id]
-    mode = (params[:mode] == Post.modes.keys[1]) ? Post.modes.keys[1] : Post.modes.keys[0]
+    mode = params[:mode]
     time = (params[:time] == Settings.post.asc) ? Settings.post.asc : Settings.post.desc
     type = params[:type] == Post.filters.keys[1] ? Post.filters.keys[1] : Post.filters.keys[0]
-    {category: category, mode: mode, time: time, type: type, domain: current_domain}
+    {category: category, mode: mode, time: time, type: type, domain: current_domain,
+      category_slug: category_slug}
   end
 
   def filtered_posts
+    posts = Post.approved
+    if filtered_param[:mode].present?
+      posts = posts.filtered_by_mode(filtered_param[:mode])
+    end
+    if filtered_param[:category].present?
+      posts = posts.filtered_by_category(filtered_param[:category].id)
+    end
     if filtered_param[:type] == Post.filters.keys[0]
-      Post.filtered_by_time_category(filtered_param[:time], filtered_param[:category].id)
-        .filtered_by_mode(filtered_param[:mode])
+      posts = posts.filtered_by_time(filtered_param[:time])
         .by_domain(filtered_param[:domain].id)
         .page(params[:page]).per Settings.common.posts_per_page
     elsif filtered_param[:type] == Post.filters.keys[1]
@@ -70,7 +77,19 @@ class Supports::Ads::PostSupport
   end
 
   def most_reviewed_post per_page = Settings.common.posts_per_page
-    Post.most_reviewed(filtered_param[:mode], filtered_param[:time],
-      filtered_param[:category].id).page(params[:page]).per per_page
+    case
+    when filtered_param[:mode].present? && filtered_param[:category].present?
+      Post.most_reviewed(filtered_param[:mode], filtered_param[:time], filtered_param[:category].id)
+        .page(params[:page]).per per_page
+    when filtered_param[:mode].present?
+      Post.most_reviewed_by_mode(filtered_param[:mode], filtered_param[:time])
+        .page(params[:page]).per per_page
+    when filtered_param[:category].present?
+      Post.most_reviewed_by_category(filtered_param[:category].id, filtered_param[:time])
+        .page(params[:page]).per per_page
+    else
+      Post.most_reviewed_order_by_time(filtered_param[:time]).page(params[:page])
+        .per per_page
+    end
   end
 end
